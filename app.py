3f6283bc22d9bf926beca0d888a5d02f44a5c264
Ali Gad <54474531+aligad1999@users.noreply.github.com>
2021-07-22 17:59:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -212,11 +212,9 @@ def get_bot_response():
             continue
 	
         inp=request.args.get('msg')
-        if(inp.lower()!="yes" or inp.lower()!="no"):
+        if (inp.lower()!="yes" or inp.lower()!="no"):
             return "Provide proper answers i.e. (yes/no) : "
-            inp=request.args.get('msg')
-
-	if(inp.lower()=="yes"):
+	if (inp.lower()=="yes"):
             symptoms_exp.append(syms)
 	
     second_prediction=sec_predict(symptoms_exp)
