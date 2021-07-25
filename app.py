c9f0b8123a447a1d0a855a5a9ca80eeb496993db
Ali Gad <54474531+aligad1999@users.noreply.github.com>
2021-07-25 07:58:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 from flask import Flask, render_template, request
 import random
+pip install python-google-places
 from googleplaces import GooglePlaces, types, lang
 import requests
 import json
